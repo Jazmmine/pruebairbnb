8a1cc406931cb750a10d287f142cda97161421ab
Jazmine Angoma <jazmine.angoma@gmail.com>
2016-12-12 02:55:08 -0500

    completando pagina

--- js/app.js
+++ js/app.js
@@ -11,7 +11,7 @@ function initMap() {
     selectYears: 15 // Creates a dropdown of 15 years to control year
   });*/
 
-  var slider = document.getElementById('rango');
+  /*var slider = document.getElementById('rango');
   noUiSlider.create(slider, {
    start: [20, 80],
    connect: true,
@@ -23,4 +23,27 @@ function initMap() {
    format: wNumb({
      decimals: 0
    })
-  });
+  });*/
+
+/* $('.carousel.carousel-slider').carousel({full_width: true});*/
+
+/*var carousel = new Carousel({
+    id: "carousel",  // id of the carousel container
+    autoplay: false, // starts the rotation automatically
+    interval: 1500,  // interval between slide changes
+    initial: 0,      // slide to start with
+    dots: true,      // show navigation dots
+    arrows: true,    // show navigation arrows
+    buttons: true    // show play/stop buttons
+});
+
+carousel.initSlide(6);*/
+
+$('#pagination-footer').materializePagination({
+  lastPage: 20,
+  firstPage:  1,
+  align: 'left', 
+  urlParameter: 'page',
+  useUrlParameter: true,
+  onClickCallback: function(){}
+});
